fb3ec37d79c399d1e6df26a3817a9a942fc62bfd
matthewadeboyejr <matthewconsult@gmail.com>
2024-01-30 20:47:26 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        overpass: ["Overpass"],
+      },
+      backgroundImage: {
+        "hero-pattern":
+          "linear-gradient(to right bottom, rgba(8, 9, 9, 0.7), rgba(8, 9, 9, 0.7)), url('https://ik.imagekit.io/et8vxrzxxdj/pexels-keira-burton-6146931__2__kg-YHWL1tkT.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
